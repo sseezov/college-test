fa31bc82913b8f6cd95c9b41bb8297af3466e7f4
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-04-19 15:18:34 +0300

    sumNestedArrays

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const array = [[12,3,4],[0,0,0],[23,5,6],[123,'6',-5],[0,'0','O',1],['test']]
+const array = [[12, 3, 4], [0, 0, 0], [23, 5, 6], [123, '6', -5], [0, '0', 'O', 1], ['test']];
 
-// Напишите функцию, которая считает сумму чисел в каждом вложенном массиве
-// и возвращает массив с суммами этих чисел в качестве значений
-// если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
-1
-// выведите итоговый массив в консоль
+function sumNestedArrays(arr) {
+  const result = [];
+
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i += 1) {
+      let sum = 0;
+      let hasNum = false;
+      for (let j = 0; j < arr[i].length; j += 1) {
+        if (typeof arr[i][j] === 'number') {
+          sum += arr[i][j];
+          hasNum = true;
+        } else if (typeof arr[i][j] === 'string') {
+          const num = parseInt(arr[i][j], 10);
+          if (!Number.isNaN(num)) {
+            sum += num;
+            hasNum = true;
+          }
+        }
+      }
+      if (hasNum) {
+        result.push([sum]);
+      } else {
+        result.push([]);
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(sumNestedArrays(array));

3600758b0f81f66d7a9092370d1e51c8f29ded27
lagzya-dev <you@example.com>
2023-04-19 13:52:01 +0300

    Super puper update

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const array = [[12, 3, 4], [0, 0, 0], [23, 5, 6], [123, '6', -5], [0, '0', 'O', 1], ['test']]
-import _ from 'lodash'
 // Напишите функцию, которая считает сумму чисел в каждом вложенном массиве
 // и возвращает массив с суммами этих чисел в качестве значений
 // если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
 // выведите итоговый массив в консоль
-function sumArray(arr) {
-    return arr.map(p => {
-        let sum = _.sumBy(p, f => isNaN(Number(f)) ? null : Number(f))
-        if (sum === null) {
-            return []
-        }
-        return [sum]
-    })
-}
+const sumArray = (arr) => arr.map(p => [p.reduce((acc, crr) => isNaN(Number(crr)) ? null : acc + Number(crr), 0)].filter(p => p != null))
 
 console.log(sumArray(array))

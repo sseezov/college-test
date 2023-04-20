8e7fd2e785bf9f26cccd8efb1c5f4caba0bbf708
DmitriyM <mironowd697@gmail.com>
2023-04-20 14:43:58 +0600

    work done

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const array = [[12,3,4],[0,0,0],[23,5,6],[123,'6',-5],[0,'0','O',1],['test']]
 // если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
 1
 // выведите итоговый массив в консоль
+
+const copiedAndSortedArr = (arr) => {
+    const result = [];
+    arr.map((inputArr) => {
+        result.push(inputArr.filter((el) => !isNaN(Number(el))))
+    })
+    return result;
+};
+
+const sum = (arr) => {
+    const normalizeArr = copiedAndSortedArr(arr);
+    const result = [];
+
+    normalizeArr.map((el) => {
+        let r = 0;
+
+        el.map((num) => {
+            r += Number(num);
+        })
+        result.push(r);
+    })
+
+    return result;
+}
+
+console.log(sum(array))

2516c5c1b54b899aef598c5d91dd21f9f62182e9
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-04-25 22:30:38 +0300

    Solution

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ const array = [
 // Напишите функцию, которая считает сумму чисел в каждом вложенном массиве
 // и возвращает массив с суммами этих чисел в качестве значений
 // если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
+
 // выведите итоговый массив в консоль
+
+const sumArr = (array) => {
+  const result = array.map((item) => {
+    return [
+      item.reduce((acc, element) => {
+        return isNaN(Number(element)) ? null : acc + Number(element);
+      }, 0),
+    ].filter((element) => element !== null);
+  });
+
+  return result;
+};
+
+console.log(sumArr(array));
